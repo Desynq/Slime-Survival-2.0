1de00e97c2ad6cd7208710f5c4bc6ca48cf1a849
Desynq <69401694+Desynq@users.noreply.github.com>
2021-11-24 23:47:57 -0500

    :rocket: Race Catalogue is finished for now.

--- js/books/race_catalogue.mjs
+++ js/books/race_catalogue.mjs
@@ -1,11 +1,11 @@
 import { truncateSync, appendFileSync } from 'fs';
 
-const outputFile = 'E:/Servers/Slime Survival 2.0/config/paxi/datapacks/js/books/output.txt';
+const outputFile = 'E:/Servers/Slime Survival 2.0/world_slime_survival/datapacks/js/books/output.txt';
 
 
 let pages = [];
 
-for (let i = 0; i < 32; i++) {
+for (let i = 0; i < 34; i++) {
 	pages.push( `'{\\"nbt\\":\\"books[{type:\\\\\\\\\\"race_catalogue\\\\\\\\\\"}].pages[${i}]\\",\\"storage\\":\\"ss:books\\",\\"interpret\\":true}'` );
 }
 
--- js/books/race_catalogue/page.mjs
+++ js/books/race_catalogue/page.mjs
@@ -3,19 +3,21 @@ const require = createRequire(import.meta.url); // because json imports are eXpe
 
 import { truncateSync, appendFileSync, readdirSync } from 'fs'; // all fs methods are synchronous because I don't want to bother with promises and async functions
 
-const mcfunction = 'E:/Servers/Slime Survival 2.0/config/paxi/datapacks/C-Slime Survival/data/ss/functions/core/load/static/books/race_catalogue.mcfunction';
+const mcfunction = 'E:/Servers/Slime Survival 2.0/world_slime_survival/datapacks/C-Slime Survival/data/ss/functions/core/load/static/books/race_catalogue.mcfunction';
 
 
 
-const files = readdirSync('E:/Servers/Slime Survival 2.0/config/paxi/datapacks/js/books/race_catalogue/json');
+const files = readdirSync('E:/Servers/Slime Survival 2.0/world_slime_survival/datapacks/js/books/race_catalogue/json');
 
 truncateSync(mcfunction);
 
 let jsonString;
 for (let i = 0; i < files.length; i++) {
 	jsonString = JSON.stringify(require(`./json/page-${i}.json`), null, 0);
+	jsonString = jsonString.replace(/\\"/g, '§');
 	jsonString = jsonString.replace(/"/g, '\\"');
 	jsonString = jsonString.replace(/\\n/g, '\\\\n');
+	jsonString = jsonString.replace(/§/g, '\\\\\\"');
 	jsonString = `"${jsonString}"`;
 	jsonString = `data modify storage ss:books books[{"type":"race_catalogue"}].pages append value ${jsonString}`;
 	appendFileSync(mcfunction,`${jsonString}\n`);
