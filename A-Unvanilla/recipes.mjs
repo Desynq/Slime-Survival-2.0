0a0d2153c4732835dcdd80b099d911c015a407d7
Desynq <69401694+Desynq@users.noreply.github.com>
2022-01-16 20:38:33 -0500

    :sparkles: Added filtering and success message to recipes.mjs • Will remove any whitespace lines

--- A-Unvanilla/recipes.mjs
+++ A-Unvanilla/recipes.mjs
@@ -3,12 +3,15 @@ import { readFileSync, unlinkSync } from "fs";
 const directory = 'D:/Servers/Slime Survival 2.0/world_slime_survival/datapacks/A-Unvanilla/data/minecraft/recipes/';
 
 let data = readFileSync('removed_recipes.txt', 'utf8').split('\r\n');
-data = data.slice(0, -1);
+
+// Remove any elements that are null, undefined, empty, or contain a comment
+data = data.filter(n => n);
 
 
 for (const file of data) {
 	try {
 		unlinkSync(`${directory}${file}.json`);
+		console.log(`Successfully removed ${file}.json`);
 	} catch {
 		console.log(`Ignoring ${file}.json`);
 	}
